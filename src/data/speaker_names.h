de03441a767a301d55b5236f4ea187e246e6bee4
StrangeOne101 <strange.toby@gmail.com>
2026-08-09 23:57:10 +1200

    Oldale and Route 103 changes
    - Made May's door and vacant house door locked with fake door tile
    - Made Latias use overworld sprite instead of fixed sprite
    - Added route 103 code to get a starter
    - Made May's battle party use Johto starters

--- src/data/speaker_names.h
+++ src/data/speaker_names.h
@@ -4,4 +4,5 @@ const u8 *const gSpeakerNamesTable[SP_NAME_COUNT] =
     [SP_NAME_DAD]    = COMPOUND_STRING("Dad"),
     [SP_NAME_PLAYER] = COMPOUND_STRING("{PLAYER}"),
     [SP_NAME_MAY]    = COMPOUND_STRING("May"),
+    [SP_NAME_MAY2]    = COMPOUND_STRING("May 2"),
 };
