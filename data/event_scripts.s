d056ca4b1d9c5ea98856da696e98424d638b5a04
StrangeOne101 <strange.toby@gmail.com>
2026-09-10 14:43:27 +1200

    Made ITM fires functional
    - Added Adams house
    - Added Miku sprite
    - Made the ITM actually work

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1315,3 +1315,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
 	.include "data/scripts/wild_encounter.inc"
+
+	.include "data/maps/RustboroCity_House3_2F/scripts.inc"
--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -268,6 +268,7 @@
 #define METATILE_General_Door_Gym              0x1CD
 #define METATILE_General_Door_PokeCenter       0x061
 #define METATILE_General_Door_PokeMart         0x041
+#define METATILE_General_FAKE_DOOR             0x1E3
 #define METATILE_General_Grass                 0x001
 #define METATILE_General_Grass_TreeLeft        0x1CE
 #define METATILE_General_Grass_TreeRight       0x1CF
